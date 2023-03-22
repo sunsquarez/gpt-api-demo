4c6b5596b36feda91c07e376b6f212d3a6fd8678
Hanson Wu <wuhonsun.hs@gmail.com>
2023-03-22 13:47:44 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Container from '@material-ui/core/Container'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { Configuration, OpenAIApi } from 'openai'
 
-function App() {
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_GPT_API_KEY,
+})
+const apiClient = new OpenAIApi(configuration)
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [response, setResponse] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = async () => {
+    if (isLoading) {
+      return
+    }
+    setResponse(null)
+    setErrorMessage(null)
+    setIsLoading(true)
+    try {
+      const ccy = inputValue || 'any'
+      const prompt = `Generate a tweet to share a thought on ${ccy} crypto in random mood with emoji`
+      const completions = await apiClient.createCompletion({
+        model: 'text-davinci-003',
+        prompt,
+        max_tokens: 880,
+        temperature: 0.7,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      })
+      setResponse(completions.data?.choices?.[0]?.text)
+    } catch (error) {
+      console.log(error)
+      setErrorMessage(error.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <h1>GPT API Demo</h1>
+        <section>
+          <TextField
+            className='input-field'
+            placeholder='Enter a crypto you want to tweet about'
+            fullWidth
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={handleSubmit}
+            disabled={isLoading}
+          >
+            Generate
+          </Button>
+        </section>
+        <section>
+          {isLoading && <CircularProgress />}
+          {response && !isLoading && (
+            <Card>
+              <CardContent>
+                {response}
+              </CardContent>
+            </Card>
+          )}
+          {errorMessage && !isLoading && (
+            <div className='error-message'>
+              {errorMessage}
+            </div>
+          )}
+        </section>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App'
+import './index.css'
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#01a781',
+    },
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </ThemeProvider>
+)
