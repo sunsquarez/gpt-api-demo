a282a68fbcb9dd6d069f8f3f5a48f2db60a4b845
Hanson Wu <wuhonsun.hs@gmail.com>
2023-03-24 18:11:47 +0800

    add reaction

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Container from '@material-ui/core/Container'
 import FormControl from '@material-ui/core/FormControl'
 import TextField from '@material-ui/core/TextField'
-import Select from '@material-ui/core/Select';
+import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
@@ -10,45 +10,17 @@ import CardContent from '@material-ui/core/CardContent'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { Configuration, OpenAIApi } from 'openai'
 
-const configuration = new Configuration({
-  apiKey: process.env.REACT_APP_GPT_API_KEY,
-})
+import { REACTIONS, LOCALES } from './constants'
+
+const configuration = new Configuration({ apiKey: process.env.REACT_APP_GPT_API_KEY })
 const apiClient = new OpenAIApi(configuration)
 
-const LOCALES = [
-  {
-    label: 'English',
-    value: 'en',
-  },
-  {
-    label: 'Pусский',
-    value: 'ru-RU',
-  },
-  {
-    label: '中文 (简化)',
-    value: 'zh-CN',
-  },
-  {
-    label: '中文 (繁體)',
-    value: 'zh-TW',
-  },
-  {
-    label: 'Español',
-    value: 'es-EM',
-  },
-  {
-    label: 'Türkçe',
-    value: 'tr-TR',
-  },
-  {
-    label: 'Português',
-    value: 'pt-BR',
-  },
-]
-
 const App = () => {
-  const [inputValue, setInputValue] = useState('')
-  const [locale, setLocale] = useState(LOCALES[0].value)
+  const [payload, setPayload] = useState({
+    crypto: '',
+    reaction: REACTIONS[0].value,
+    locale: LOCALES[0].value,
+  })
   const [response, setResponse] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
@@ -61,8 +33,12 @@ const App = () => {
     setErrorMessage(null)
     setIsLoading(true)
     try {
-      const ccy = inputValue || 'any'
-      const prompt = `Generate a tweet to share a thought on ${ccy} crypto in random mood with emoji in ${locale}`
+      const {
+        crypto = 'any',
+        reaction,
+        locale,
+      } = payload
+      const prompt = `Generate a tweet to share a thought on ${crypto} crypto in ${reaction} reaction with emoji in ${locale}`
       const completions = await apiClient.createCompletion({
         model: 'text-davinci-003',
         prompt,
@@ -86,20 +62,30 @@ const App = () => {
       <Container maxWidth='sm'>
         <h1>GPT-3 AI Writer</h1>
         <section>
-          <FormControl fullWidth>
+          <FormControl>
             <TextField
               className='input-field'
               placeholder='Enter a crypto you want to tweet about'
               fullWidth
-              onChange={(e) => setInputValue(e.target.value)}
+              onChange={(e) => setPayload({ ...payload, crypto: e.target.value})}
             />
+            <Select
+              value={payload.reaction}
+              onChange={(e) => setPayload({ ...payload, reaction: e.target.value})}
+            >
+              {
+                REACTIONS.map((item) => (
+                  <MenuItem value={item.value} key={item.value}>
+                    {item.label}
+                  </MenuItem>
+                ))
+              }
+            </Select>
           </FormControl>
           <FormControl fullWidth>
             <Select
-              value={locale}
-              onChange={(e) => {
-                setLocale(e.target.value)
-              }}
+              value={payload.locale}
+              onChange={(e) => setPayload({ ...payload, locale: e.target.value})}
             >
               {
                 LOCALES.map((item) => (
---
+++ src/constants.js
@@ -0,0 +1,57 @@
+export const LOCALES = [
+  {
+    label: 'English',
+    value: 'en',
+  },
+  {
+    label: 'Pусский',
+    value: 'ru-RU',
+  },
+  {
+    label: '中文 (简化)',
+    value: 'zh-CN',
+  },
+  {
+    label: '中文 (繁體)',
+    value: 'zh-TW',
+  },
+  {
+    label: 'Español',
+    value: 'es-EM',
+  },
+  {
+    label: 'Türkçe',
+    value: 'tr-TR',
+  },
+  {
+    label: 'Português',
+    value: 'pt-BR',
+  },
+]
+
+export const REACTIONS = [
+  {
+    label: '👍',
+    value: 'like',
+  },
+  {
+    label: '❤️',
+    value: 'love',
+  },
+  {
+    label: '😂',
+    value: 'haha',
+  },
+  {
+    label: '😯',
+    value: 'wow',
+  },
+  {
+    label: '😢',
+    value: 'sad',
+  },
+  {
+    label: '😡',
+    value: 'angry',
+  },
+]
